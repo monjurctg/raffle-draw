1acb876b446ed4c26fcdad301d1d43e51b888375
monjurpy.06 <monjurpy.06@gmail.com>
2022-06-03 18:01:52 +0600

    routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-// router.use("/api/v1/tickets", require("../routes/ticket"));
+router.use("/api/v1/tickets", require("../routes/ticket"));
 
 router.get("/health", (_req, res) => {
   res.status(200).json({ message: "Success" });
